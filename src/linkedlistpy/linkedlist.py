3a98412f3f27f774e29e037213c22be36637aeef
Paul Pan <76851428+packetsss@users.noreply.github.com>
2021-09-28 15:14:20 -0700

    added DoublyLinkedList + tests

--- src/linkedlistpy/linkedlist.py
+++ src/linkedlistpy/linkedlist.py
@@ -6,6 +6,7 @@ class Node:
     def __init__(self, data: Any) -> None:
         self.data = data
         self.next: Optional[Node] = None
+        self.prev: Optional[Node] = None
 
 
 class LinkedList:
@@ -74,7 +75,7 @@ class LinkedList:
 
     def delete(self, value: Any) -> None:
         temp = self.head
-
+        prev = None
         while temp is not None:
             if temp.data == value:
                 break
@@ -82,9 +83,11 @@ class LinkedList:
             temp = temp.next
         if temp is None:
             return
-
-        prev.next = temp.next
-        temp = None
+        if prev is None and self._length == 1:
+            self.head = Node(None)
+        else:
+            prev.next = temp.next
+            temp = None
         self._length -= 1
 
     def reverse(self) -> None:
@@ -98,12 +101,151 @@ class LinkedList:
         self.head = prev
 
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = Node(None)
+        self.tail = Node(None)
+        self._length = 0
+
+    def __len__(self) -> int:
+        return self._length
+
+    def __iter__(self) -> Iterable[Any]:
+        curr = self.head
+        if curr.data is None:
+            return iter([])
+
+        output_list = []
+        while curr:
+            output_list.append(curr.data)
+            curr = curr.next
+
+        return iter(output_list)
+
+    def __repr__(self) -> str:
+        return list(self.__iter__()).__repr__()
+
+    def append(self, data: Any) -> None:
+        if not isinstance(data, Iterable) or isinstance(data, str):
+            data = [data]
+        for d in data:
+            new_node = Node(d)
+            if not self.head.data:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.prev = self.tail
+                self.tail.next = new_node
+                self.tail = new_node
+            self._length += 1
+
+    def append_left(self, data: Any) -> None:
+        if not isinstance(data, Iterable) or isinstance(data, str):
+            data = [data]
+
+        data = reversed(data)
+        for d in data:
+            new_node = Node(d)
+            if not self.tail.data:
+                print(1)
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head.prev = new_node
+                self.head = new_node
+                print(self.head.data)
+            self._length += 1
+
+    def insert(self, index: int, value: Any) -> None:
+        index = max(-self._length, min(index, self._length))
+        if index == 0:
+            self.append_left(value)
+            return
+        elif index < 0:
+            index = self._length + index + 1
+        if index == self._length:
+            self.append(value)
+            return
+
+        curr = self.head
+        new_node = Node(value)
+        for _ in range(index - 1):
+            curr = curr.next
+        new_node.next = curr.next
+        curr.next = new_node
+        new_node.prev = curr
+        self._length += 1
+
+    def delete(self, value: Any) -> None:
+        curr = self.head
+        if curr.data is None:
+            return
+        if curr.data == value:
+            if self._length == 1:
+                self.head = Node(None)
+                self.tail = Node(None)
+            else:
+                self.head = curr.next
+                self.head.prev = None
+        elif self.tail.data == value:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            while curr:
+                if curr.data == value:
+                    curr.prev.next = curr.next
+                    curr.next.prev = curr.prev
+                    break
+                curr = curr.next
+            else:
+                return
+        self._length -= 1
+
+    def reverse(self):
+        # Node curr will point to head
+        curr = self.head
+        
+        # Swap the previous and next pointers of each node to reverse the direction of the list
+        while curr != None:
+            temp = curr.next
+            curr.next = curr.prev
+            curr.prev = temp
+            curr = curr.prev
+            
+        # Swap the head and tail pointers.
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+    def pop(self, index: int = -1) -> Any:
+        curr = self.head
+        index = max(-self._length, min(index, self._length))
+        if index == 0:
+            value = self.head.data
+            self.head = curr.next
+            self.head.prev = None
+            return value
+        elif index < 0:
+            index = self._length + index + 1
+        if index == self._length:
+            return
+        temp = self.tail
+        if not temp:
+            return
+        data = temp.data
+        self.tail = temp.prev
+        if not self.tail:
+            self.head = None
+        else:
+            self.tail.next = None
+        return data
+
+
 class CircularLinkedList:
     def __init__(self) -> None:
         self.head = Node(None)
         self.tail = Node(None)
-        self.head.next = self.tail
-        self.tail.next = self.head
         self._length = 0
 
     def __len__(self) -> int:
@@ -176,10 +318,13 @@ class CircularLinkedList:
     def delete(self, value: Any) -> None:
         curr = self.head
         prev = None
-        while True:
+        while 1:
             # found
             if curr.data == value:
-                if prev is not None:
+                if self._length == 1:
+                    self.head = Node(None)
+                    self.tail = Node(None)
+                elif prev is not None:
                     prev.next = curr.next
                 else:
                     while curr.next != self.head:
--- tests/test_linkedlist.py
+++ tests/test_linkedlist.py
@@ -1,5 +1,5 @@
 import pytest
-from linkedlistpy import LinkedList, CircularLinkedList
+from linkedlistpy import LinkedList, DoublyLinkedList, CircularLinkedList
 
 linked_list_data = [3, 23.234, -21.3, "e", False, tuple]
 
@@ -13,25 +13,32 @@ def test_linked_list():
     assert list(linked_list) == linked_list_data
     assert str(linked_list) == linked_list_data.__str__()
     assert len(linked_list) == len(linked_list_data)
-    
+
     # test insert
     linked_list.insert(2, 4)
     linked_list.insert(0, "23")
     linked_list.insert(-1, True)
-    assert list(linked_list) == ['23', 3, 23.234, 4, -21.3, 'e', False, tuple, True]
+    assert list(linked_list) == ["23", 3, 23.234, 4, -21.3, "e", False, tuple, True]
     assert len(linked_list) == 9
 
     # test delete
     linked_list.delete(3)
     linked_list.delete(tuple)
     linked_list.delete(1212)
-    assert list(linked_list) == ['23', 23.234, 4, -21.3, 'e', False, True]
+    assert list(linked_list) == ["23", 23.234, 4, -21.3, "e", False, True]
     assert len(linked_list) == len(linked_list_data) + 1
 
     # test empty
     another_ll = LinkedList()
     assert another_ll.head.data is None
     assert list(another_ll) == []
+
+    # test edge case
+    a = LinkedList()
+    a.append_left(1)
+    assert a.head.data == 1
+    a.delete(1)
+    assert a.head.data == None
 
     # test append
     another_ll.append(3)
@@ -47,14 +54,73 @@ def test_linked_list():
     assert list(another_ll) == [8.7, 2, 3, 3, 1.2, -8.8]
 
 
-# another_cll = CircularLinkedList()
-# another_cll.append([7, 4, 1, 0, 44, 12])
-# print(another_cll)
-# another_cll.insert(2, 4)
-# another_cll.insert(0, "23")
-# another_cll.insert(-1, True)
-# print(another_cll)
-# print(another_cll.head.data, another_cll.tail.next.data)
+def test_doubly_linked_list():
+    # general test
+    doubly_linked_list = DoublyLinkedList()
+    doubly_linked_list.append(linked_list_data)
+    assert doubly_linked_list.head.data == linked_list_data[0]
+    assert doubly_linked_list.head.next.data == linked_list_data[1]
+    assert list(doubly_linked_list) == linked_list_data
+    assert str(doubly_linked_list) == linked_list_data.__str__()
+    assert len(doubly_linked_list) == len(linked_list_data)
+
+    # test insert
+    doubly_linked_list.insert(2, 4)
+    doubly_linked_list.insert(0, "23")
+    doubly_linked_list.insert(-1, True)
+    assert list(doubly_linked_list) == [
+        "23",
+        3,
+        23.234,
+        4,
+        -21.3,
+        "e",
+        False,
+        tuple,
+        True,
+    ]
+    assert len(doubly_linked_list) == 9
+    assert doubly_linked_list.tail.prev.data == tuple
+    assert doubly_linked_list.head.next.data == 3
+
+    # test delete
+    doubly_linked_list.delete(3)
+    doubly_linked_list.delete(tuple)
+    doubly_linked_list.delete(1212)
+    doubly_linked_list.delete(True)
+    assert list(doubly_linked_list) == ["23", 23.234, 4, -21.3, "e", False]
+    assert len(doubly_linked_list) == len(linked_list_data)
+    assert doubly_linked_list.tail.prev.data == "e"
+    assert doubly_linked_list.head.next.data == 23.234
+
+    # test empty
+    another_dll = DoublyLinkedList()
+    another_dll.delete(3)
+    assert another_dll.head.data is None
+    assert list(another_dll) == []
+
+    # test edge case
+    a = DoublyLinkedList()
+    a.append_left(1)
+    assert a.head.data == a.tail.data == 1
+    a.delete(1)
+    assert a.head.data == a.tail.data == None
+
+    # test append
+    another_dll.append(1)
+    another_dll.append([4, 7])
+    another_dll.append_left([44, 0])
+    another_dll.append_left(12)
+    assert another_dll.head.data == 12
+    assert another_dll.tail.prev.data == 4
+    assert list(another_dll) == [12, 44, 0, 1, 4, 7]
+    assert len(another_dll) == 6
+
+    # test reverse
+    another_dll.reverse()
+    assert list(another_dll) == [7, 4, 1, 0, 44, 12]
+    assert another_dll.tail.prev.data == 44
+    assert another_dll.head.next.data == 4
 
 
 def test_circular_linked_list():
@@ -76,15 +142,21 @@ def test_circular_linked_list():
     assert len(circular_linked_list) == 4
     assert circular_linked_list.tail.data == tuple
     assert circular_linked_list.tail.next == circular_linked_list.head
-    
+
     # test empty
     another_cll = CircularLinkedList()
     another_cll.reverse()
     assert another_cll.head.data is None
     assert list(another_cll) == []
 
+    # test edge case
+    a = CircularLinkedList()
+    a.append_left(1)
+    assert a.head.data == 1
+    a.delete(1)
+    assert a.head.data == a.tail.data == None
+
     # test append
-    CircularLinkedList().append_left(1)
     another_cll.append(1)
     another_cll.append([4, 7])
     another_cll.append_left([44, 0])
@@ -94,19 +166,18 @@ def test_circular_linked_list():
     assert list(another_cll) == [12, 44, 0, 1, 4, 7]
     assert len(another_cll) == 6
 
-    # testreverse
+    # test reverse
     another_cll.reverse()
     assert list(another_cll) == [7, 4, 1, 0, 44, 12]
     assert another_cll.tail.data == 12
     assert another_cll.tail.next == another_cll.head
-    
+
     # test insert
     another_cll.insert(2, 4)
     another_cll.insert(0, "23")
     another_cll.insert(-1, True)
-    assert list(another_cll) == ['23', 7, 4, 4, 1, 0, 44, 12, True]
+    assert list(another_cll) == ["23", 7, 4, 4, 1, 0, 44, 12, True]
     assert another_cll.head.data == another_cll.tail.next.data
-
 
 
 """
